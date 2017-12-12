bc329fe723c6779be8aff8acf4bfc89c3a3e18ad
jmonne3 <jmonne3@xtec.cat>
2017-12-12 17:17:50 +0100

    Afegim arrays

--- index.php
+++ index.php
@@ -4,4 +4,21 @@
   print "Hello World<br><br>";#comentari de línea
   echo "Hola" . "Món" . "<br>";
   $salt = "<br>";
+  $num = 5;
+  $num .= 5;//=num=num+5
+  echo "Hello World" . $salt . "HE SALTAT!";
+  echo "$num" . $salt;
+  echo '$num' . $salt;
+
+  $myArray = array("Hola", 5, true);
+  echo $myArray[0] . $salt;
+  echo $myArray[1] . $salt;
+  echo $myArray[2] . $salt;
+
+  for ($i = 0 ; $i < 3; $i++){
+    echo $myArray[$i] . $salt;
+  }
+
+ print_r($myArray);
+
  ?>
