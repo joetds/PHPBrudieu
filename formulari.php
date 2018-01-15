3f46e6e1a878d6b8043f672b0cd00f37623c0cbf
Joel Monné <jmonne3@xtec.cat>
2018-01-15 21:26:05 +0100

    inici activitat formulari

---
+++ formulari.php
@@ -0,0 +1,15 @@
+<html>
+  <form method="get">
+    <input type="text" name="nom" placeholder="Name"></input>
+    <input type="text" name="cognom" placeholder="Surname"></input> <br>
+    <br>
+    <input type="text" name="user" placeholder="User"></input>
+      <input type="radio" name="man" value="Man"> Man
+      <input type="radio" name="woman" value="Woman"> Woman
+    <br><br>
+    <input type="password" name="pass" placeholder="Password"></input>
+    <input type="password" name="pass2" placeholder="Repeteat Password"></input> <br>
+    <input type="submit" name="submit" value="envia"></input>
+    <br>
+  </form>
+</html>
