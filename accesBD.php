247be0010820bb4318c34429eecf98c261ece352
jmonne3 <jmonne3@xtec.cat>
2018-01-19 19:20:18 +0100

    connexió amb la BD

---
+++ accesBD.php
@@ -0,0 +1,30 @@
+
+
+<?
+//host, usuari, contrasenya, nom BD
+$connexio = mysqli_connect('localhost','root','alumne','exemple');ex
+
+if(!$connexio){die("ERROR" . mysqli_connect_error());}
+
+$queryCreateTable = "CREATE TABLE `mmo` (`id` INT NOT NULL AUTO_INCREMENT ,
+        `joc` VARCHAR(255) NOT NULL ,
+        `nivell` INT(3) NOT NULL ,
+        PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+
+mysqli_query($connexio,$queryCreateTable);
+
+$queryInsert = "INSERT INTO `mmo` (`joc`, `nivell`) VALUE ('joc1', '4');";
+
+mysqli_query($connexio,$queryInsert);
+
+$queryInsert = "INSERT INTO `mmo` (`joc`, `nivell`) VALUE ('joc2', '15');";
+
+mysqli_query($connexio,$queryInsert);
+
+$querySelect = "SELECT * FROM `mmo`;";
+$result = mysqli_query($connexio,$querySelect);
+while($row = mysqli_fetch_assoc($result) ){
+  echo "id: " . $row["id"] . " joc: " . $row["joc"] . " nivell: " . $row["nivell"] . "<br>";
+}
+mysqli_close($connexio);
+?>
